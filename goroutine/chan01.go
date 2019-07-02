30f95120cbe1765cc64083e406af3f07415dcf61
mhy2011 <mhy2011@163.com>
2019-07-02 10:15:32 +0800

    add chan example

---
+++ goroutine/chan01.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("start main")
+	strCh := make(chan string)
+	go producer(strCh)  //启动生产者
+	go consumer(strCh)  //启动消费者
+	time.Sleep(6 * 1e9) //休眠6s
+	fmt.Println("end main")
+}
+
+func producer(ch chan string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("ready to produce msg, i  = %d\n", i)
+		ch <- fmt.Sprintf("hello_%d", i)
+	}
+}
+
+func consumer(ch chan string) {
+	for {
+		data := <-ch
+		fmt.Println("consume data :", data)
+	}
+}
